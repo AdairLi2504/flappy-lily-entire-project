cd7825186c771f1ea7ad9a1c970829b4a4447c9b
flyingsnoopy <lijiaqi1835@outlook.com>
2024-12-08 23:12:56 +0800

    12_8

---
+++ script/genbackground/extend.py
@@ -0,0 +1,7 @@
+from PIL import Image
+
+backgroundUnit = Image.open("./background.png")
+image = Image.new("RGB",(2160,1360),color="black")
+for i in range(0,3):
+    image.paste(backgroundUnit,(0+i*720,0))
+image.save("background.png")
---
+++ script/genbackground/main.py
@@ -0,0 +1,22 @@
+from PIL import Image
+
+def addBackground(base: Image.Image) -> Image.Image:
+    forest = Image.open("./backgroundColorForest.png")
+    forest = forest.resize((720, int(forest.size[1] * 720 / forest.size[0])))
+    base.paste(forest,(0,420))
+    return base
+
+def addGround(base: Image.Image) -> Image.Image:
+    grassMid = Image.open("./grassMid.png")
+    grassCentre = Image.open("./grassCenter.png")
+    for i in range(0,11):
+        base.paste(grassMid,(i*70,940))
+        for j in range(0,6):
+            base.paste(grassCentre,(i*70,1010+j*70))
+    return base
+
+
+entireBackground = Image.new("RGB",(720,1360),color=(207,239,252))
+entireBackground = addBackground(entireBackground)
+entireBackground = addGround(entireBackground)
+entireBackground.save("background.png")
